4f17dc1049932e219b0575c57d86b1e187a3ebf5
Aman Gupta <56467452+amangupta679@users.noreply.github.com>
2022-01-16 18:36:00 +0530

    flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,28 @@
+import 'dart:html';
 
+import 'package:flutter/material.dart';
+import 'Home_page.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Calculator App",
+      theme: new ThemeData(primarySwatch: Colors.red),
+      home: new HomePage(),
+    );
+  }
+}
+
+// @override
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.amber,
+    
+//     );
+//   }
+// }
